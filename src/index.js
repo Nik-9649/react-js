2b047c6295b7a4e3f0897ab4e9d3769caef8a5bf
Nik-9649 <usmanfazaljj@gmail.com>
2022-08-09 17:56:55 +0500

    JSX Index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = "Click me!";
+  return (
+    <div>
+      <label for="name" className="label">
+        enter e-mail
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "cyan", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
